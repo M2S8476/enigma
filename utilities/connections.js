6dc2118e81439441399ba0a67ea1c2e6d6e4ad49
Maulik Sangani <mauliksangani8476@gmail.com>
2023-09-14 11:45:33 +0530

    commit

--- utilities/connections.js
+++ utilities/connections.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 let mongoDB = mongoose.createConnection(process.env.MONGO_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology: false
+	useUnifiedTopology: true,
 });
 module.exports = mongoDB;
